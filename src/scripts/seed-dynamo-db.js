cf68588bb3e20b6f9bba0959a0f21242c2aa3c75
wjs <wojciech.strzelecki@packhelp.com>
2021-04-18 13:34:57 +0200

    dynamo batch write chunks

--- src/scripts/seed-dynamo-db.js
+++ src/scripts/seed-dynamo-db.js
@@ -2,8 +2,11 @@ import {
   DynamoDBClient,
   BatchWriteItemCommand,
 } from "@aws-sdk/client-dynamodb";
+import chunk from "lodash/chunk";
 import { generateFakeData } from "./fake-data-generator";
+
 const REGION = "eu-central-1";
+const MAX_BATCH_SIZE = 25; // dynamo-db limtation
 
 const TABLES = {
   blogs: {
@@ -30,87 +33,101 @@ const TABLES = {
 
 const dbclient = new DynamoDBClient({ region: REGION });
 
-export const seedDynamoDb = async () => {
-  const json = generateFakeData();
-  try {
-    const blogs = json.blogs.map((blog) => ({
-      PutRequest: {
-        Item: {
-          id: { S: blog._id },
-          name: { S: blog.name },
-          __typename: { S: TABLES.blogs.typename },
-          createdAt: { S: new Date().toISOString() },
-          updatedAt: { S: new Date().toISOString() },
-        },
+export const seedDynamoDb = async (json) => {
+  const blogs = json.blogs.map((blog) => ({
+    PutRequest: {
+      Item: {
+        id: { S: blog._id },
+        name: { S: blog.name },
+        __typename: { S: TABLES.blogs.typename },
+        createdAt: { S: new Date().toISOString() },
+        updatedAt: { S: new Date().toISOString() },
       },
-    }));
+    },
+  }));
 
-    const posts = json.posts.map((post) => ({
-      PutRequest: {
-        Item: {
-          id: { S: post._id },
-          title: { S: post.title },
-          postBlogId: { S: post.postBlogId },
-          __typename: { S: TABLES.posts.typename },
-          createdAt: { S: new Date().toISOString() },
-          updatedAt: { S: new Date().toISOString() },
-        },
+  const posts = json.posts.map((post) => ({
+    PutRequest: {
+      Item: {
+        id: { S: post._id },
+        title: { S: post.title },
+        postBlogId: { S: post.postBlogId },
+        __typename: { S: TABLES.posts.typename },
+        createdAt: { S: new Date().toISOString() },
+        updatedAt: { S: new Date().toISOString() },
       },
-    }));
+    },
+  }));
 
-    const comments = json.comments.map((comment) => ({
-      PutRequest: {
-        Item: {
-          id: { S: comment._id },
-          content: { S: comment.content },
-          commentPostId: { S: comment.commentPostId },
-          __typename: { S: TABLES.comments.typename },
-          createdAt: { S: new Date().toISOString() },
-          updatedAt: { S: new Date().toISOString() },
-        },
+  const comments = json.comments.map((comment) => ({
+    PutRequest: {
+      Item: {
+        id: { S: comment._id },
+        content: { S: comment.content },
+        commentPostId: { S: comment.commentPostId },
+        __typename: { S: TABLES.comments.typename },
+        createdAt: { S: new Date().toISOString() },
+        updatedAt: { S: new Date().toISOString() },
       },
-    }));
+    },
+  }));
 
-    const users = json.users.map((user) => ({
-      PutRequest: {
-        Item: {
-          id: { S: user._id },
-          username: { S: user.username },
-          __typename: { S: TABLES.users.typename },
-          createdAt: { S: new Date().toISOString() },
-          updatedAt: { S: new Date().toISOString() },
-        },
+  const users = json.users.map((user) => ({
+    PutRequest: {
+      Item: {
+        id: { S: user._id },
+        username: { S: user.username },
+        __typename: { S: TABLES.users.typename },
+        createdAt: { S: new Date().toISOString() },
+        updatedAt: { S: new Date().toISOString() },
       },
-    }));
+    },
+  }));
 
-    const posteditors = json.posteditors.map((posteditor) => ({
-      PutRequest: {
-        Item: {
-          id: { S: posteditor._id },
-          editorId: { S: posteditor.editorId },
-          postId: { S: posteditor.postId },
-          __typename: { S: TABLES.posteditors.typename },
-          createdAt: { S: new Date().toISOString() },
-          updatedAt: { S: new Date().toISOString() },
-        },
+  const posteditors = json.posteditors.map((posteditor) => ({
+    PutRequest: {
+      Item: {
+        id: { S: posteditor._id },
+        editorId: { S: posteditor.editorId },
+        postId: { S: posteditor.postId },
+        __typename: { S: TABLES.posteditors.typename },
+        createdAt: { S: new Date().toISOString() },
+        updatedAt: { S: new Date().toISOString() },
       },
-    }));
+    },
+  }));
 
-    const params = {
-      RequestItems: {
-        [TABLES.blogs.name]: blogs,
-        [TABLES.posts.name]: posts,
-        [TABLES.comments.name]: comments,
-        [TABLES.users.name]: users,
-        [TABLES.posteditors.name]: posteditors,
-      },
-    };
+  const blogsChunks = chunk(blogs, MAX_BATCH_SIZE);
+  const postsChunks = chunk(posts, MAX_BATCH_SIZE);
+  const usersChunks = chunk(users, MAX_BATCH_SIZE);
+  const commentsChunks = chunk(comments, MAX_BATCH_SIZE);
+  const posteditorsChunks = chunk(posteditors, MAX_BATCH_SIZE);
 
-    const data = await dbclient.send(new BatchWriteItemCommand(params));
-    console.log("Success, items inserted", data);
-  } catch (err) {
-    console.log("Error", err);
+  const chunks = {
+    [TABLES.blogs.name]: blogsChunks,
+    [TABLES.posts.name]: postsChunks,
+    [TABLES.comments.name]: commentsChunks,
+    [TABLES.users.name]: usersChunks,
+    [TABLES.posteditors.name]: posteditorsChunks,
+  };
+
+  const chunkKeys = Object.keys(chunks);
+
+  for (let i = 0; i < chunkKeys.length; i++) {
+    const tableName = chunkKeys[i];
+
+    for (let i = 0; i < chunks[tableName].length; i++) {
+      try {
+        const params = {
+          RequestItems: {
+            [tableName]: chunks[tableName][i],
+          },
+        };
+        const data = await dbclient.send(new BatchWriteItemCommand(params));
+        console.log("Success, items inserted", data);
+      } catch (err) {
+        console.log("Error", err);
+      }
+    }
   }
 };
-
-seedDynamoDb();
