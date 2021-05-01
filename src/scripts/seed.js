c1e2c037bf709b4a96ebd9f07bf0d8fa394d2b3c
wjs <wojciech.strzelecki@packhelp.com>
2021-05-01 18:59:23 +0200

    build script

--- src/scripts/seed.js
+++ src/scripts/seed.js
@@ -2,7 +2,7 @@ import { generateFakeData } from "./fake-data-generator";
 import { seedMongoDb } from "./seed-mongo-db";
 import { seedDynamoDb } from "./seed-dynamo-db";
 
-export const seed = async (json) => {
+export const seed = async () => {
   const json = generateFakeData();
   await seedMongoDb(json);
   await seedDynamoDb(json);
